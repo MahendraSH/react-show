78102d4ec0e4c4d83ec7b8611fb9799e2b813826
MahendraSH <maahi2001sh@gmail.com>
2024-02-01 16:56:59 +0530

    updated : font

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Home from "./pages/Home";
 import Shows from "./pages/Shows";
 import PageNotFound from "./pages/Page-not-found";
 import ShowIdPage from "./pages/Show-id-page";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 
 export default function App() {
   return (
